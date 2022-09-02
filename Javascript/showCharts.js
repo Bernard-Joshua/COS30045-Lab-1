f82dbaa07fdecd7671c840e07fb0ab67cd5ef81e
Bernard Joshua Raja Rajan <67912425+Bernard-Joshua@users.noreply.github.com>
2022-09-02 20:10:34 +0800

    Update showCharts.js

--- Javascript/showCharts.js
+++ Javascript/showCharts.js
@@ -7,18 +7,20 @@ let figCap = document.getElementById("chart-cap");
 
 // Adding the logic.
 // When the button is clicked then the figure and figure caption should   change.
+// Using "addEventListener" helps seperate the Javascript from the HTML by attaching an event to the DOM.
 
-btnNineteen.addEventListener('click',function() {
-    imgChart.src = "/Images/pets2019.jpg";
-    figCap.innerHTML = "Fig 2. Visualisation of pet trends in 2019."
-} )
+btnNineteen.addEventListener("click", function () {
+	imgChart.src = "/Images/pets2019.jpg";
+	figCap.innerHTML = "Fig 2. Visualisation of pet trends in 2019.";
+});
 
-btnTwenty.addEventListener('click',function() {
-    imgChart.src = "/Images/pets2021.jpg";
-    figCap.innerHTML = "Fig 3. Visualisation of pet trends in 2021."
-} )
+btnTwenty.addEventListener("click", function () {
+	imgChart.src = "/Images/pets2021.jpg";
+	figCap.innerHTML = "Fig 3. Visualisation of pet trends in 2021.";
+});
 
-btnBoth.addEventListener('click',function() {
-    imgChart.src = "/Images/pets2019vs2021.jpg";
-    figCap.innerHTML = "Fig 4. Visualisation of comparison of pet trends in 2019 and 2021."
-} )
+btnBoth.addEventListener("click", function () {
+	imgChart.src = "/Images/pets2019vs2021.jpg";
+	figCap.innerHTML =
+		"Fig 4. Visualisation of comparison of pet trends in 2019 and 2021.";
+});
